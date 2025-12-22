84387026a5bbc951d3135297ac69e0bf10e1cbce
MusaviMariam <mariam.musvi@gmail.com>
2025-12-22 20:47:42 +0100

    added visualization scipt for graph

--- learner/visualize_llg.py
+++ learner/visualize_llg.py
@@ -14,25 +14,72 @@ MODEL_FILE = "blocks_llg_mean_8.dt"
 # === 1. Build the graph ===
 llg = LiftedLearningGraph(domain_pddl=DOMAIN_FILE, problem_pddl=PROBLEM_FILE)
 llg._compute_graph_representation()
-G = llg.G
+#G = llg.G
+# IMPORTANT: initialise c_graph for visualization
+llg.c_graph = llg.G.copy()
+
 print("Nodes",len(llg.G.nodes),"Edges", len(llg.G.edges))
 
+# === 1.5. Add initial state to the graph ===
+initial_state = [
+    ("handempty", ()),
+    ("on-table", ("b1",)),
+    ("on-table", ("b2",)),
+    ("clear", ("b1",)),
+    ("clear", ("b2",)),
+]
+
+G = llg.state_to_cgraph(initial_state)
+
 # === 2. Prepare node colors ===
-node_colors = []
-for node in G.nodes:
+#node_colors = []
+#for node in G.nodes:
     # Default color
-    color = "skyblue"
+#    color = "skyblue"
     
     # Goal nodes
-    if hasattr(llg, "_pos_goal_nodes") and node in llg._pos_goal_nodes:
-        color = "green"
-    elif hasattr(llg, "_neg_goal_nodes") and node in llg._neg_goal_nodes:
-        color = "red"
+#    if hasattr(llg, "_pos_goal_nodes") and node in llg._pos_goal_nodes:
+#        color = "green"
+#    elif hasattr(llg, "_neg_goal_nodes") and node in llg._neg_goal_nodes:
+#        color = "red"
     
     # Activated propositions can be colored differently if you have a state
     # Here we just highlight goal nodes
     
-    node_colors.append(color)
+ #   node_colors.append(color)
+
+ # === 2. Prepare node colors (Figure 3 style) ===
+object_nodes = {obj.name for obj in llg.problem.objects}
+predicate_nodes = {pred.name for pred in llg.problem.predicates}
+action_nodes = {action.name for action in llg.problem.actions}
+
+node_colors = []
+
+for node in G.nodes:
+    # --- GOAL ---
+    if node in llg._pos_goal_nodes:
+        node_colors.append("green")
+    
+    # --- STATE ---
+    elif node not in llg.G.nodes:
+        node_colors.append("yellow")
+    
+    # --- OBJECT ---
+    elif node in object_nodes:
+        node_colors.append("skyblue")
+    
+    # --- PREDICATE ---
+    elif node in predicate_nodes:
+        node_colors.append("hotpink")
+    
+    # --- ACTION ---
+    elif node in action_nodes:
+        node_colors.append("orange")
+    
+    # --- AUX / VAR ---
+    else:
+        node_colors.append("lightgray")
+
 
 # === 3. Prepare edge colors based on LLG edge types ===
 edge_colors = []
@@ -45,6 +92,8 @@ for u, v, data in G.edges(data=True):
         edge_colors.append("red")
     elif label == LLG_EDGE_LABELS["ground"]:
         edge_colors.append("blue")
+    #elif label == LLG_EDGE_LABELS["mutex"]:
+     #   edge_colors.append("purple")  # Distinct color for FAM groups    
     else:
         edge_colors.append("gray")
 
